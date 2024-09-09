31f5a672f78e91e565dbaf44bbb38aa55f248c21
bhumika-poriya <bhumikaporiya16@gmail.com>
2024-09-09 11:30:40 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './angular-icon.svg';
 import './App.css';
+import logo from './logo.svg'
 
 function App() {
+
+  let name = "library";
+  let arr = [10, 20, 30, 40];
+  let obj = {
+    name: "Sagar",
+    cname: "Second",
+    desc: "Student",
+  }
+
+  let status = false;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Forks..
-        </p>
-        <a
-          className="App-link"
-          href="https://angularjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn angular
-        </a>
-      </header>
+      <h2>{name}</h2>
+      {
+        arr.map((v) => {
+          return (
+            <h2>{v}</h2>
+          )
+        })
+      }
+      <div><img src={logo} className="App-logo" alt="logo" /></div>
+
+      <div>{obj.cname}</div>
+      {10 + 50}
+
+      {(status) ? <h1 style={{ color: 'blueviolet' }}>Welcome to home page</h1> : <h1 style={{ color: 'red' }}>Welcome to home page</h1>}
+
     </div>
   );
 }
